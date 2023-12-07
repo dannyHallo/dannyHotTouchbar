340c1845b9882804d7156f37bba7a411efb11eb4
DannyHallo <ruitian0716@gmail.com>
2023-12-07 11:31:27 +0800

    renaming

--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,16 @@ import * as vscode from "vscode";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "danny-hot-touchbar" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  // let disposable = vscode.commands.registerCommand('danny-hot-touchbar.helloWorld', () => {
-  // 	// The code you place here will be executed every time your command is executed
-  // 	// Display a message box to the user
-  // 	vscode.window.showInformationMessage('Hello World from danny-hot-touchbar!');
-  // });
-
-  let disposable = vscode.commands.registerCommand("extension.compileAndRun", () => {
+  let compileAndRunDisposable = vscode.commands.registerCommand("extension.compileAndRun", () => {
     vscode.commands.executeCommand("terminator.run", "xmake and run 1010");
   });
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(compileAndRunDisposable);
+
+  let renameDisposable = vscode.commands.registerCommand("extension.renaming", () => {
+    // Simulate pressing the F2 key
+    vscode.commands.executeCommand("editor.action.rename");
+  });
+  context.subscriptions.push(renameDisposable);
 }
 
 // This method is called when your extension is deactivated
